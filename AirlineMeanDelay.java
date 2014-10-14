f32cc96415256fd8232d7bd02286d3cf44baa62f
ambegh <ambegh@gmail.com>
2014-10-14 10:21:13 +0200

    Average delay of each airline monthly (in minutes)
    This hadoop code does calculate the average of departure delay per
    month.
    Mapper output - < Pair , delay> where Pair is the month concerted with
    airline
    Reducer output <Pair , average (delay)>

---
+++ AirlineMeanDelay.java
@@ -0,0 +1,86 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.KeyValueTextInputFormat;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+
+public class trial {
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
+		
+		
+		@Override
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, DoubleWritable> output, Reporter reporter)
+				throws IOException {
+			
+			String inputLine = value.toString();
+            String pattern = "(,\"[^\"]+),(.+\")";
+   			String preprocessed = inputLine.replaceAll(pattern , "$1~$2");	   
+    	    
+    	    String[] tokens = preprocessed.split(",");	              
+            String airline_month = tokens[2] + "_" + tokens[6] ;
+            double delayMin;  
+        	
+        	if (! tokens[0].startsWith("\"Year\"")){	
+			  if ( ! tokens[27].isEmpty()){
+			  delayMin = Double.parseDouble(tokens[27]);
+			  output.collect(new Text(airline_month), new DoubleWritable(delayMin) );			
+		     }
+		    }
+		}		
+	}
+	
+	public static class Reduce extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+
+		@Override
+		public void reduce(Text key, Iterator<DoubleWritable> values,
+				OutputCollector<Text, DoubleWritable> output, Reporter reporter)
+				throws IOException {
+			
+			
+			double sum = 0;
+			int count = 0;
+			while(values.hasNext()) {
+				
+				sum += values.next().get();
+			    count += 1;	   
+			 }
+			
+			output.collect(key, new DoubleWritable(sum/count));
+		}		
+	
+	}
+	
+	public static void main(String[] args) throws IOException {
+		JobConf conf = new JobConf(trial.class);
+		conf.setJobName("mean");
+		
+		conf.setMapperClass(Map.class);
+		conf.setReducerClass(Reduce.class);
+		conf.setMapOutputKeyClass(Text.class);
+		conf.setMapOutputValueClass(DoubleWritable.class);
+		
+		//conf.setInputFormat(KeyValueTextInputFormat.class);
+		//conf.set("key.value.separator.in.input.line", " ");
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(DoubleWritable.class);
+		
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+	      
+		JobClient.runJob(conf);
+	}
+}
